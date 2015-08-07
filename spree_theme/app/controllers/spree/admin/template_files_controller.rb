057af38b6ddbd3bcf3cade5a2c4a9e0264fe1a60
zhang ze <david@www.david.com>
2015-08-07 22:51:02 +0800

    improve template_file pagination

--- spree_theme/app/controllers/spree/admin/template_files_controller.rb
+++ spree_theme/app/controllers/spree/admin/template_files_controller.rb
@@ -1,11 +1,20 @@
 module Spree
   module Admin
     class TemplateFilesController < Spree::Admin::ResourceController
-      
-      
-      def collection
-        model_class.where( ["theme_id in (?)", Spree::Site.current.template_theme_ids] ).includes(:template_theme)        
+
+      def show
+        session[:return_to] ||= request.referer
+        redirect_to action: :edit
       end
-    end    
+
+      def index
+        session[:return_to] = request.url
+        respond_with(@collection)
+      end
+
+      def collection
+        model_class.where( ["theme_id in (?)", Spree::Site.current.template_theme_ids] ).order("theme_id").page(params[:page]).per(Spree::Config[:admin_products_per_page])
+      end
+    end
   end
-end 
+end
