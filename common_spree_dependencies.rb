24e6b4622164756d528d7bb77cd7caa26a0aa27f
qinghe <david@getstore.cn>
2017-12-13 01:43:53 -0500

    upgrade spree 3.2

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -1,2 +1,2 @@
-gem 'spree', '~> 3.1' #, :github => "spree/spree", :branch => "3-1-stable"
-gem 'spree_auth_devise', '~> 3.1' #, :github => "spree/spree_auth_devise", :branch => "3-1-stable"
+gem 'spree', '~> 3.2.0' #, :github => "spree/spree", :branch => "3-1-stable"
+gem 'spree_auth_devise', '~> 3.2.0' #, :github => "spree/spree_auth_devise", :branch => "3-1-stable"
